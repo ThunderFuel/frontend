72b3e545c1f940be0fb87103c9bcd36ccf26b2a3
musaakkayaa <musaakkayaa@gmail.com>
2023-01-13 02:29:24 +0300

    FRO-11 refactor UAT

--- src/pages/Collection/pages/Items/components/Filter/index.tsx
+++ src/pages/Collection/pages/Items/components/Filter/index.tsx
@@ -4,18 +4,30 @@ import InputSearch from "components/InputSearch";
 import InputRange from "components/InputRange";
 import Input from "components/Input";
 import { IconClear } from "icons";
+import clsx from "clsx";
 
 const Range = () => {
   const [rangeValue, setRangeValue] = React.useState(0);
 
   return (
     <div className="flex gap-5 px-5 py-2.5 border-r border-r-gray">
-      <div className="flex flex-col flex-1 gap-2">
-        <div className="text-headline-01 uppercase text-gray-light">Sweeper</div>
-        <InputRange value={rangeValue} minValue={0} maxValue={100} onChange={(e) => setRangeValue(e as number)} />
+      <div className="flex flex-col justify-center flex-1 gap-2">
+        <div className="text-headline-01 uppercase text-gray-light">Sweep</div>
+        <div className="overflow-hidden px-1.5 -mx-1.5 rounded-full">
+          <InputRange value={rangeValue} minValue={0} maxValue={100} onChange={(e) => setRangeValue(e as number)} />
+        </div>
       </div>
       <div>
-        <Input value={rangeValue} icon={<IconClear className="w-6 h-6 mr-2 cursor-pointer" />} className="w-10 border-l border-l-gray pl-4" placeholder={"0"} />
+        <Input
+          value={rangeValue}
+          onChange={(e: any) => {
+            const value = e.target.value;
+            setRangeValue(value < 0 ? 0 : value > 100 ? 100 : value);
+          }}
+          icon={<IconClear className="w-6 h-6 mr-2 cursor-pointer" />}
+          className={clsx("w-10 border-l border-l-gray pl-4", rangeValue > 0 ? "text-white" : "text-gray")}
+          placeholder={"0"}
+        />
       </div>
     </div>
   );
