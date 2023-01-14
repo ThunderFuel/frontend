c89a8e874aa353c4350cf310b4b6b866f5486289
musaakkayaa <musaakkayaa@gmail.com>
2023-01-14 15:23:00 +0300

    FRO-18 uat note

--- src/pages/Marketplace/HotDrops.tsx
+++ src/pages/Marketplace/HotDrops.tsx
@@ -1,35 +1,43 @@
 import React from "react";
 import { AssetHotdrop1Image } from "assets";
-import { IconEthereum } from "icons";
 import { useIsMobile } from "hooks/useIsMobile";
+import EthereumPrice from "components/EthereumPrice";
+import { chunk } from "../../utils";
+import Carousel from "components/Carousel";
 
 const HotDrops = () => {
-  const images = useIsMobile() ? [1] : [1, 2, 3, 4];
+  const chunkSize = useIsMobile() ? 1 : 4;
+  const imagesList = chunk([1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4], chunkSize);
 
   return (
     <div className="container-fluid flex flex-col gap-5">
       <h3 className="text-white text-h3">Hot Drops</h3>
-      <div className="flex flex-col lg:flex-row gap-3">
-        {images.map((item, k) => {
+      <Carousel>
+        {imagesList.map((images, k) => {
           return (
-            <div key={k} className="overflow-hidden border border-gray bg-bg-light text-white">
-              <div className="overflow-hidden">
-                <img src={AssetHotdrop1Image} className="w-full transition-all ease-in duration-300 hover:scale-[120%]" alt={"hot-drop-1"} />
+            <Carousel.Item key={k}>
+              <div className="grid lg:grid-cols-4 gap-3">
+                {images.map((image: any) => {
+                  return (
+                    <div key={`${k}_${image}`} className="border border-gray bg-bg-light text-white cursor-pointer">
+                      <div className="overflow-hidden">
+                        <img src={AssetHotdrop1Image} className="w-full transition-all ease-in duration-300 hover:scale-[120%]" alt={"hot-drop-1"} />
+                      </div>
+                      <div className="flex flex-col gap-3 px-4 py-3">
+                        <h5 className="text-head5 font-spaceGrotesk">Presence by Tompop</h5>
+                        <div className="flex items-center gap-2.5">
+                          <span className="text-headline-01 uppercase text-gray-light">floor:</span>
+                          <EthereumPrice price={1.31} />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
               </div>
-              <div className="flex flex-col gap-3 px-4 py-3">
-                <h5 className="text-head5 font-spaceGrotesk">Presence by Tompop</h5>
-                <div className="flex items-center gap-2.5">
-                  <span className="text-headline-01 uppercase text-gray-light">floor:</span>
-                  <h5 className="text-h5 flex items-center">
-                    1.31
-                    <IconEthereum className={"text-gray-light"} />
-                  </h5>
-                </div>
-              </div>
-            </div>
+            </Carousel.Item>
           );
         })}
-      </div>
+      </Carousel>
     </div>
   );
 };
