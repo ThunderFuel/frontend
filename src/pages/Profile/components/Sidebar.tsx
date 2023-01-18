846eb1a7398712f25f960998dfbe44b6b6cb5d29
musaakkayaa <musaakkayaa@gmail.com>
2023-01-19 01:30:46 +0300

    FRO-21 create profile page

--- src/pages/Profile/components/Sidebar.tsx
+++ src/pages/Profile/components/Sidebar.tsx
@@ -6,6 +6,13 @@ import SocialButtons from "../../Collection/components/SocialButtons";
 import "./Sidebar.css";
 import { IconCirclePlus } from "icons";
 
+const CoverImage = () => {
+  return (
+    <div className="profile-cover-image">
+      <Img src={AssetProfileBg} className="w-full" />
+    </div>
+  );
+};
 const LogoContainer = () => {
   const socialMedias = [
     { type: 0, url: "http://www.azuki.com/" },
@@ -31,15 +38,45 @@ const LogoContainer = () => {
   );
 };
 
+const Box = () => {
+  return (
+    <div className="grid grid-cols-2 bg-bg border border-gray rounded-md mt-5">
+      <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light border-r border-r-gray">
+        <div className="text-headline-01 uppercase">followers</div>
+        <h4 className="text-h4 text-white">12</h4>
+      </div>
+      <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light">
+        <div className="text-headline-01 uppercase">FOLLOWING</div>
+        <h4 className="text-h4 text-white">1,223</h4>
+      </div>
+    </div>
+  );
+};
+const Box1 = () => {
+  return (
+    <div className="grid grid-cols-2 gap-2 mt-2">
+      <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md">
+        <div className="text-headline-01 uppercase">followers</div>
+        <h4 className="text-h4 text-white text-right mt-20">12</h4>
+      </div>
+      <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md">
+        <div className="text-headline-01 uppercase">FOLLOWING</div>
+        <h4 className="text-h4 text-white text-right mt-20">1,223</h4>
+      </div>
+    </div>
+  );
+};
+
 const Sidebar = () => {
   return (
     <div className="flex flex-col border-r border-gray w-[500px]">
-      <div className="profile-cover-image">
-        <Img src={AssetProfileBg} className="w-full" />
-      </div>
-      <div className="p-10 relative -translate-y-52 text-white">
-        <div className="flex">
+      <CoverImage />
+      <div className="p-10 relative -translate-y-[272px] text-white">
+        <div className="flex flex-col">
           <LogoContainer />
+          <div className="body-medium mt-5">Gm! I’m xero and I like to collect superb NFTs</div>
+          <Box />
+          <Box1 />
         </div>
       </div>
     </div>
