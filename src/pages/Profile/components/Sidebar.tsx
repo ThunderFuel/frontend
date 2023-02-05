1b5977b7b6b4b5edd945fd3654668d823c987b81
musaakkayaa <musaakkayaa@gmail.com>
2023-02-05 22:50:49 +0300

    FRO-21 update sidebar

--- src/pages/Profile/components/Sidebar.tsx
+++ src/pages/Profile/components/Sidebar.tsx
@@ -53,9 +53,9 @@ BoxWithIcon.displayName = "BoxWithIcon";
 const Sidebar = ({ userInfo, openFollowers, openFollows }: any) => {
   return (
     <div className="flex flex-col border-r border-gray w-[500px]">
-      <div className="sticky top-[110px]">
+      <div className="sidebar-container">
         <CoverImage />
-        <div className="p-10 relative -translate-y-[272px] text-white">
+        <div className="p-10 relative pt-[150px] text-white">
           <div className="flex flex-col">
             <LogoContainer userName={userInfo?.userName} socialMedias={userInfo?.socialMedias} />
             <div className="body-medium mt-5">Gm! I’m xero and I like to collect superb NFTs</div>
@@ -69,23 +69,23 @@ const Sidebar = ({ userInfo, openFollowers, openFollows }: any) => {
                 <h4 className="text-h4 text-white">{numberFormat(userInfo.follows?.length)}</h4>
               </div>
             </div>
-            <div className="grid grid-cols-2 gap-2 mt-2">
-              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md">
+            <div className="grid flex-1 grid-cols-2 gap-2 mt-2">
+              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md justify-between">
                 <div className="text-headline-01 uppercase">lısted/owned</div>
-                <h4 className="text-h4 text-white text-right mt-20">{numberFormat(userInfo.tokens?.length)}</h4>
+                <h4 className="text-h4 text-white text-right mt-2">{numberFormat(userInfo.tokens?.length)}</h4>
               </div>
-              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md">
+              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md justify-between">
                 <div className="text-headline-01 uppercase">created</div>
-                <h4 className="text-h4 text-white text-right mt-20">{numberFormat(userInfo.tokens?.length)}</h4>
+                <h4 className="text-h4 text-white text-right mt-2">{numberFormat(userInfo.tokens?.length)}</h4>
               </div>
             </div>
             <BoxWithIcon icon={IconQuarry} className="mt-2">
-              <div className="text-headline-02 text-gray-light uppercase">LAST offer</div>
-              0.99 ETH Bid placed by 09x910
+              <div className="text-headline-01 text-gray-light uppercase">LAST offer</div>
+              <h6 className="text-h6">0.99 ETH Bid placed by 09x910</h6>
             </BoxWithIcon>
             <BoxWithIcon icon={IconQuarry} className="mt-2">
-              <div className="text-headline-02 text-gray-light uppercase">LAST ACTIVITY</div>
-              Minted CloneX #2750
+              <div className="text-headline-01 text-gray-light uppercase">LAST ACTIVITY</div>
+              <h6 className="text-h6">Minted CloneX #2750</h6>
             </BoxWithIcon>
           </div>
         </div>
