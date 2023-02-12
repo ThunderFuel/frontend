9341901c7ab25029d4f416ef50d4767302fa72d3
musaakkayaa <musaakkayaa@gmail.com>
2023-02-12 16:13:33 +0300

    FRO-63

--- src/pages/Profile/components/Sidebar/Sidebar.tsx
+++ src/pages/Profile/components/Sidebar/Sidebar.tsx
@@ -4,39 +4,35 @@ import "./Sidebar.css";
 import { IconQuarry } from "icons";
 import { numberFormat } from "utils";
 import LogoContainer from "./components/LogoContainer";
-import BoxWithIcon from "./components/BoxWithIcon";
+import { Box, BoxWithIcon } from "./components/Box";
 import CoverImage from "./components/CoverImage";
 import { ButtonEdit, ButtonFollow } from "./components/Buttons";
 
 const Sidebar = ({ userInfo, openFollowers, openFollows, onChangeFollowers, isProfile = false }: any) => {
   return (
-    <div className="flex flex-col border-r border-gray w-[500px]">
+    <div className="flex flex-col border-r border-gray min-w-[500px] max-w-[500px]">
       <div className="sidebar-container">
         {isProfile ? <ButtonEdit /> : <ButtonFollow userInfo={userInfo} onChangeFollowers={onChangeFollowers} />}
         <CoverImage />
         <div className="p-10 relative pt-[150px] text-white w-full h-full flex">
           <div className="flex flex-col w-full">
             <LogoContainer userName={userInfo?.userName} socialMedias={userInfo?.socialMedias} userId={userInfo?.id} contractAddress={userInfo.contractAddress} />
-            <div className="body-medium mt-5">Gm! I’m xero and I like to collect superb NFTs</div>
-            <div className="grid grid-cols-2 bg-bg border border-gray rounded-md mt-5">
-              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light border-r border-r-gray cursor-pointer" onClick={openFollowers}>
-                <div className="text-headline-01 uppercase">followers</div>
-                <h4 className="text-h4 text-white">{numberFormat(userInfo.followers?.length)}</h4>
-              </div>
-              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light cursor-pointer" onClick={openFollows}>
-                <div className="text-headline-01 uppercase">FOLLOWING</div>
-                <h4 className="text-h4 text-white">{numberFormat(userInfo.follows?.length)}</h4>
-              </div>
+            <div className="body-medium mt-5">{userInfo?.bio}</div>
+            <div className="grid grid-cols-2 gap-2 mt-5">
+              <Box header="followers" onClick={openFollowers}>
+                {numberFormat(userInfo.followers?.length)}
+              </Box>
+              <Box header="FOLLOWING" onClick={openFollows}>
+                {numberFormat(userInfo.follows?.length)}
+              </Box>
             </div>
             <div className="grid flex-1 grid-cols-2 gap-2 mt-2">
-              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md justify-between">
-                <div className="text-headline-01 uppercase">lısted/owned</div>
-                <h4 className="text-h4 text-white text-right mt-2">{numberFormat(userInfo.tokens?.length)}</h4>
-              </div>
-              <div className="flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md justify-between">
-                <div className="text-headline-01 uppercase">created</div>
-                <h4 className="text-h4 text-white text-right mt-2">{numberFormat(userInfo.tokens?.length)}</h4>
-              </div>
+              <Box header="lısted/owned" className="justify-between">
+                <div className="text-right">{numberFormat(userInfo.tokens?.length)}</div>
+              </Box>
+              <Box header="created" className="justify-between">
+                <div className="text-right">{numberFormat(userInfo.tokens?.length)}</div>
+              </Box>
             </div>
             <BoxWithIcon icon={IconQuarry} className="mt-2">
               <div className="text-headline-01 text-gray-light uppercase">LAST offer</div>
--- src/pages/Profile/components/Sidebar/components/Box.tsx
+++ src/pages/Profile/components/Sidebar/components/Box.tsx
@@ -1,6 +1,23 @@
 import React, { SVGProps } from "react";
 import clsx from "clsx";
 
+interface IBox {
+  header: string;
+  onClick?: () => void;
+  className?: string;
+  children: any;
+}
+
+const Box = React.memo(({ header, onClick, className, children }: IBox) => {
+  return (
+    <div className={clsx("flex flex-col gap-2 px-4 py-3.5 text-gray-light bg-bg border border-gray rounded-md cursor-pointer", className)} onClick={onClick}>
+      <div className="text-headline-01 uppercase">{header}</div>
+      <h4 className="text-h4 text-white">{children}</h4>
+    </div>
+  );
+});
+Box.displayName = "Box";
+
 const BoxWithIcon = React.memo(({ children, className, icon }: { children: React.ReactNode; className?: string; icon: React.FC<SVGProps<SVGSVGElement>> }) => {
   const Icon = icon;
 
@@ -15,4 +32,4 @@ const BoxWithIcon = React.memo(({ children, className, icon }: { children: React
 });
 BoxWithIcon.displayName = "BoxWithIcon";
 
-export default BoxWithIcon;
+export { Box, BoxWithIcon };
