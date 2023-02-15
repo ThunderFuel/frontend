19544a2bc6751c17c18e939d47e75355f51b5239
okanakademirr <okanakdemirr@gmail.com>
2023-02-16 02:57:51 +0300

    wip

--- src/pages/Marketplace/Marketplace.tsx
+++ src/pages/Marketplace/Marketplace.tsx
@@ -3,15 +3,21 @@ import HotDrops from "./HotDrops";
 import MarketplaceList from "./MarketplaceList";
 import Filter from "./Filter/index";
 import MarketplaceProvider from "./MarketplaceContext";
+import { useIsMobile } from "hooks/useIsMobile";
+import MobileWarning from "components/MobileWarning";
 
 const Marketplace = () => {
-  return (
+  return !useIsMobile() ? (
     <div className="flex flex-col gap-20 py-16">
       <HotDrops />
       <MarketplaceProvider>
         <Filter />
         <MarketplaceList itemCount={10} />
       </MarketplaceProvider>
+    </div>
+  ) : (
+    <div className="m-5">
+      <MobileWarning />
     </div>
   );
 };
