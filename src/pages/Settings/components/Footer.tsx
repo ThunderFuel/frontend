2b94d2764a742db84350d55fc53e5c86aafa8a86
musaakkayaa <musaakkayaa@gmail.com>
2023-02-18 00:58:43 +0300

    FRO-102 added button

--- src/pages/Settings/components/Footer.tsx
+++ src/pages/Settings/components/Footer.tsx
@@ -1,20 +1,30 @@
 import React from "react";
 import Button from "components/Button";
-import { IconPencil } from "icons";
+import { IconCircleRemoveWhite, IconPencil } from "icons";
 import { EventSettingsSubmit } from "../Settings";
+import useNavigate from "hooks/useNavigate";
+import { PATHS } from "router/config/paths";
 
 const Footer = () => {
+  const navigate = useNavigate();
   const onSubmit = () => {
     window.dispatchEvent(new CustomEvent(EventSettingsSubmit));
+  };
+
+  const onCancel = () => {
+    navigate(PATHS.PROFILE);
   };
 
   return (
     <div className="sticky bottom-0 bg-bg px-32 border-t border-gray">
       <div className="flex border-x border-gray">
         <div className="w-[320px] border-r border-gray" />
-        <div className="flex px-10 py-5">
+        <div className="flex px-10 py-5 gap-2.5">
           <Button className="" onClick={onSubmit}>
             SAVE CHANGES <IconPencil />
+          </Button>
+          <Button className="btn-secondary" onClick={onCancel}>
+            CANCEL <IconCircleRemoveWhite />
           </Button>
         </div>
       </div>
