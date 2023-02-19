b19b1477e31b91d2c16e95eb2e53c033dd3b5bb0
musaakkayaa <musaakkayaa@gmail.com>
2023-02-19 20:00:23 +0300

    FRO-126, cosmetic

--- src/pages/Profile/components/Sidebar/components/LogoContainer.tsx
+++ src/pages/Profile/components/Sidebar/components/LogoContainer.tsx
@@ -9,12 +9,14 @@ const LogoContainer = ({ userInfo }: any) => {
   const { user } = useAppSelector((state) => state.wallet) as any;
   const isFollowers = userInfo?.follows?.find((follower: any) => follower.userId === user?.id);
 
+  const walletAddress = addressFormat(userInfo?.walletAddress);
+
   return (
     <div className="flex gap-5 w-full">
       <Avatar image={userInfo?.image} userId={user?.id} className="w-[84px] h-[84px]" />
       <div className="flex flex-col gap-2.5 flex-1 text-gray-light">
         <div className="flex items-center gap-2">
-          <h3 className="text-h3 text-white">{userInfo?.userName}</h3>
+          <h3 className="text-h3 text-white">{userInfo?.userName ?? walletAddress}</h3>
           {isFollowers ? (
             <div className="flex items-center gap-2">
               <IconCirclePlus className="" />
@@ -24,7 +26,7 @@ const LogoContainer = ({ userInfo }: any) => {
         </div>
         <div className="flex gap-2">
           <div className="border border-gray rounded-[4px] px-2 py-1.5">
-            <span className="body-medium">{addressFormat(userInfo?.walletAddress)}</span>
+            <span className="body-medium">{walletAddress}</span>
           </div>
           <SocialButtons socialMedias={userInfo?.socialMedias ?? []} />
         </div>
