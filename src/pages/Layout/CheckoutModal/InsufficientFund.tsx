9cb57c4080e45438ff255e073998c85707253079
okanakademirr <okanakdemirr@gmail.com>
2023-02-20 12:46:01 +0300

    Insufficient Funds - mock datas removed

--- src/pages/Layout/CheckoutModal/InsufficientFund.tsx
+++ src/pages/Layout/CheckoutModal/InsufficientFund.tsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "components/Button";
 import Modal from "components/Modal";
 import EthereumPrice from "components/EthereumPrice";
 import { IconWarning } from "icons";
+import { useWallet } from "hooks/useWallet";
+import { formatDisplayedNumber } from "utils";
+import { useAppSelector } from "store";
 
 const InsufficientFunds = ({ show, onClose }: { show: boolean; onClose: any }) => {
+  const { getBalance } = useWallet();
+  const { totalAmount } = useAppSelector((state) => state.cart);
+  const [balance, setbalance] = useState<number>(0);
+
+  function fetchBalance() {
+    getBalance().then((res) => setbalance(res ? res : 0));
+  }
+
+  useEffect(() => {
+    fetchBalance();
+  }, []);
+
+  const neededAmount = (totalAmount - balance / 1000000000).toFixed(2);
+
   const footer = (
     <div className="flex flex-col w-full h-full items-center">
       <div className="flex w-full py-2 px-5 justify-between border-b border-gray">
         <span className="text-h6 text-gray-light">Your Balance</span>
-        <EthereumPrice className="text-white" price={1.2} />
+        <EthereumPrice className="text-white" price={formatDisplayedNumber(balance)} />
       </div>
       <div className="flex flex-col w-full gap-y-2.5 p-5">
         <Button>ADD FUNDS</Button>
@@ -28,7 +45,7 @@ const InsufficientFunds = ({ show, onClose }: { show: boolean; onClose: any }) =
         </div>
         <div className="flex flex-col">
           <h5 className="text-h5 text-white">
-            You need <span className="text-h5 text-red">4.2 ETH + gas fees.</span>
+            You need <span className="text-h5 text-red">{neededAmount} ETH + gas fees.</span>
           </h5>
           <span className="body-medium text-gray-light">Transfer funds to your wallet or add funds with a card. It can take up to a minute for your balance to update.</span>
         </div>
