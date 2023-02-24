41962c49ff47deb59ec14b7fa94bac90f0530012
musaakkayaa <musaakkayaa@gmail.com>
2023-02-24 23:16:28 +0300

    FRO-186, cosmetic.

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -5,11 +5,12 @@ import { Outlet } from "react-router-dom";
 import Tab from "./components/Tab";
 import ProfileProvider from "./ProfileContext";
 import { useAppSelector } from "store";
+import ConnectWalletScreen from "components/ConnectWalletScreen";
 
 const Profile = () => {
   const { user } = useAppSelector((state) => state.wallet);
 
-  return (
+  return user.id ? (
     <ProfileProvider userId={user.id ?? 16} options={{ isProfile: true, isUserPage: true }}>
       <div className="flex">
         <Sidebar isProfile={true} />
@@ -20,6 +21,8 @@ const Profile = () => {
         <ModalSocial />
       </div>
     </ProfileProvider>
+  ) : (
+    <ConnectWalletScreen />
   );
 };
 
