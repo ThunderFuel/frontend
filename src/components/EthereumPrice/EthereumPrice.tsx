413561f044697c98701913b899bb31f6da0e922b
okanakdemirr <okanakdemirr@gmail.com>
2023-02-25 19:45:59 +0300

    ethereumprice price format updated

--- src/components/EthereumPrice/EthereumPrice.tsx
+++ src/components/EthereumPrice/EthereumPrice.tsx
@@ -5,9 +5,16 @@ import clsx from "clsx";
 const EthereumPrice = ({ price, priceClassName, className }: { price: any; priceClassName?: string; className?: string }) => {
   const hasPrice = !!price;
 
+  const formatPrice = (price: number) => {
+    if (typeof price === "string") return price;
+    const formattedNum = price.toFixed(4).replace(/\.?0+$/, "");
+
+    return formattedNum;
+  };
+
   return (
     <div className={clsx("flex items-center", className)}>
-      <h6 className={clsx(priceClassName ? priceClassName : "text-h5", !hasPrice && "text-gray-light")}>{price ? parseFloat(price).toFixed(2) : "-"}</h6>
+      <h6 className={clsx(priceClassName ? priceClassName : "text-h5", !hasPrice && "text-gray-light")}>{price ? formatPrice(price) : "-"}</h6>
       <IconEthereum className="text-gray-light" />
     </div>
   );
