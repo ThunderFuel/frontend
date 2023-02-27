8568b087b66e3b054a3e9995601504ef9d3bdd31
okanakdemirr <okanakdemirr@gmail.com>
2023-02-27 22:03:56 +0300

    FRO-261

--- src/pages/Profile/pages/Offer/components/SidebarFilter.tsx
+++ src/pages/Profile/pages/Offer/components/SidebarFilter.tsx
@@ -9,24 +9,26 @@ const SidebarFilter = () => {
 
   return (
     <div className="py-5 pr-5 border-r border-gray w-60">
-      <Collapse isOpen={true}>
-        <Collapse.Header>Offer Type</Collapse.Header>
-        <Collapse.Body>
-          {filterItems.map((item: any, k: any) => {
-            const isChecked = item.value === filterValue;
+      <div className="sticky h-fit" style={{ top: "calc(var(--headerHeight) + 20px)" }}>
+        <Collapse isOpen={true}>
+          <Collapse.Header>Offer Type</Collapse.Header>
+          <Collapse.Body>
+            {filterItems.map((item: any, k: any) => {
+              const isChecked = item.value === filterValue;
 
-            return (
-              <div key={k} className={clsx("hover:bg-bg-light border border-gray rounded-md p-2.5 text-white", isChecked ? "bg-gray" : "")}>
-                <Radio defaultChecked={isChecked} onClick={() => onChangeFilterValue(item.value)} name="offer-type">
-                  <span className="body-medium">
-                    {item.text} {item.count > 0 ? `(${item.count})` : null}
-                  </span>
-                </Radio>
-              </div>
-            );
-          })}
-        </Collapse.Body>
-      </Collapse>
+              return (
+                <div key={k} className={clsx("hover:bg-bg-light border border-gray rounded-md p-2.5 text-white", isChecked ? "bg-gray" : "")}>
+                  <Radio defaultChecked={isChecked} onClick={() => onChangeFilterValue(item.value)} name="offer-type">
+                    <span className="body-medium">
+                      {item.text} {item.count > 0 ? `(${item.count})` : null}
+                    </span>
+                  </Radio>
+                </div>
+              );
+            })}
+          </Collapse.Body>
+        </Collapse>
+      </div>
     </div>
   );
 };
