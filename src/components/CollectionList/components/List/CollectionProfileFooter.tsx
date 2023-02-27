1054b3dcff964f29b3d57e396f167234b4b44a9a
okanakdemirr <okanakdemirr@gmail.com>
2023-02-27 20:36:14 +0300

    FRO-264

--- src/components/CollectionList/components/List/CollectionProfileFooter.tsx
+++ src/components/CollectionList/components/List/CollectionProfileFooter.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Button from "components/Button";
-import { IconTag } from "icons";
+import { IconTag, IconTrash } from "icons";
 import { useAppDispatch, useAppSelector } from "store";
 import useNavigate from "hooks/useNavigate";
 import { PATHS } from "router/config/paths";
@@ -26,7 +26,7 @@ const CollectionFooter = () => {
     <div className="sticky bottom-0 py-3 flex gap-3 items-center justify-end border-t border-t-gray bg-bg z-20">
       <Button className="btn-secondary uppercase" onClick={onClear}>
         Clear {selectedBulkListingItemCount} {selectedBulkListingItemCount > 1 ? "items" : "item"}
-        <IconTag />
+        <IconTrash />
       </Button>
       <Button className="uppercase" onClick={onUpdateBulkListing}>
         LIST/ UPDATE {selectedBulkListingItemCount} {selectedBulkListingItemCount > 1 ? "items" : "item"}
