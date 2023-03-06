d72f66abe23b4853eda6210aeec8c693b5d82da7
musaakkayaa <musaakkayaa@gmail.com>
2023-03-06 22:25:04 +0300

    FRO-302, cosmetic

--- src/pages/Collection/components/SocialButtons.tsx
+++ src/pages/Collection/components/SocialButtons.tsx
@@ -1,12 +1,15 @@
 import React, { useState } from "react";
 import clsx from "clsx";
 
-import { IconDiscord, IconInstagram, IconShare, IconSocial3Dots, IconStar, IconTelegram, IconTwitter, IconWeblink, IconYoutube } from "icons";
+import { IconDiscord, IconInstagram, IconLink, IconShare, IconSocial3Dots, IconStar, IconTelegram, IconTwitter, IconWeblink, IconYoutube } from "icons";
 import { SocialTypes } from "api/collections/collections.type";
-import collectionsService from "../../../api/collections/collections.service";
-import { useAppSelector } from "../../../store";
+import collectionsService from "api/collections/collections.service";
+import { useAppSelector } from "store";
 import { useDispatch } from "react-redux";
 import { toggleWalletModal } from "store/walletSlice";
+import { useClickOutside } from "hooks/useClickOutside";
+import { clipboardCopy, openInNewTab } from "utils";
+import useToast from "hooks/useToast";
 
 const SocialButton = ({ icon, className, ...etc }: { icon: any; className?: string; [key: string]: any }) => {
   const IconItem = icon ?? null;
@@ -56,6 +59,55 @@ const FavoriteButton = ({ collection }: { collection: any }) => {
     </li>
   );
 };
+
+const SocialShareButton = () => {
+  const [show, setShow] = React.useState(false);
+  const containerRef = React.useRef<HTMLLIElement>(null);
+  const items = [
+    {
+      text: "Copy Link",
+      icon: IconLink,
+      onClick: () => {
+        clipboardCopy(window.location.href);
+        useToast().success("Copied to clipboard.");
+        setShow(false);
+      },
+    },
+    {
+      text: "Share on Twitter",
+      icon: IconTwitter,
+      onClick: () => {
+        openInNewTab("https://twitter.com/intent/tweet?text=Hello%20world");
+        setShow(false);
+      },
+    },
+  ];
+  useClickOutside(containerRef, () => {
+    setShow(false);
+  });
+
+  return (
+    <li className="relative" ref={containerRef}>
+      <div className={clsx("px-2 py-2  hover:text-white cursor-pointer", show ? "text-white bg-bg-light" : "text-gray-light")} onClick={() => setShow(!show)}>
+        <IconShare />
+      </div>
+      {show ? (
+        <ul className="absolute right-0 top-full mt-1 flex flex-col bg-bg border border-gray rounded-[4px] divide-y divide-gray overflow-hidden">
+          {items.map((item, k) => {
+            const Icon = item.icon;
+
+            return (
+              <li key={k} onClick={item.onClick} className="flex w-[200px] items-center justify-between cursor-pointer px-4 py-3 text-white hover:bg-bg-light">
+                <span className="flex w-full body-medium whitespace-nowrap">{item.text}</span>
+                <Icon />
+              </li>
+            );
+          })}
+        </ul>
+      ) : null}
+    </li>
+  );
+};
 const SocialButtons = ({ socialMedias, collection }: { socialMedias: { url: string; type: SocialTypes }[] | null; collection?: any }) => {
   const iconList: any = {
     [SocialTypes.Website]: IconWeblink,
@@ -78,7 +130,7 @@ const SocialButtons = ({ socialMedias, collection }: { socialMedias: { url: stri
       ) : null}
       <ul className="inline-flex border border-gray rounded-[4px]">
         {collection ? <FavoriteButton collection={collection} /> : null}
-        <SocialButton href="#" icon={IconShare} className={"border-r-0"} />
+        <SocialShareButton />
       </ul>
     </div>
   );
