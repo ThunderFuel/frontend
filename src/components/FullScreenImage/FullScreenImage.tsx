c62dea873860aa7b57d50e3c40c2fd16b2ca1e5e
okanakdemirr <okanakdemirr@gmail.com>
2023-03-13 12:02:52 +0300

    FRO-360 done

--- src/components/FullScreenImage/FullScreenImage.tsx
+++ src/components/FullScreenImage/FullScreenImage.tsx
@@ -6,12 +6,10 @@ import React from "react";
 const FullScreenImage = ({ image, onClose, show }: { image: string; onClose: any; show: boolean }) => {
   return (
     <Modal onClose={onClose} show={show}>
-      <div className="w-full h-full">
-        <Img src={image} className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2  h-full max-h-full max-w-full" />
-        <div className="absolute top-7 right-7">
-          <div className="border border-gray rounded-md p-2 group cursor-pointer bg-bg-light group" onClick={onClose}>
-            <IconExitFullscreen className="text-gray-light group-hover:text-white" />
-          </div>
+      <Img src={image} className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 object-contain max-h-full max-w-full" />
+      <div className="absolute top-7 right-7">
+        <div className="border border-gray rounded-md p-2 group cursor-pointer bg-bg-light group" onClick={onClose}>
+          <IconExitFullscreen className="text-gray-light group-hover:text-white" />
         </div>
       </div>
     </Modal>
