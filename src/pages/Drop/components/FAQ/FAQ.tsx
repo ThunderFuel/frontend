c76175452708dae5d649b4c94b0d679632f30122
musaakkayaa <musaakkayaa@gmail.com>
2023-05-16 21:57:40 +0300

    FRO-1, cosmetic

---
+++ src/pages/Drop/components/FAQ/FAQ.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { useDropDetailContext } from "../../DropContext";
+import Collapse from "components/Collapse/Collapse";
+import clsx from "clsx";
+
+const FaqItem = ({ item }: any) => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+  const onOpenCallBack = (value: boolean) => {
+    setIsActive(value);
+  };
+
+  return (
+    <Collapse className={clsx("border-white border-opacity-10 transition-all", isActive && "bg-white bg-opacity-10")} onOpenCallBack={onOpenCallBack}>
+      <Collapse.Header>
+        <h5 className="text-h5">{item.title}</h5>
+      </Collapse.Header>
+      <Collapse.Body>
+        <div className="body-medium text-white text-opacity-50">{item.text}</div>
+      </Collapse.Body>
+    </Collapse>
+  );
+};
+const Faq = () => {
+  const { dropDetail } = useDropDetailContext();
+
+  return (
+    <div className="flex flex-col gap-5 w-[800px]">
+      {dropDetail.faq.map((item: any, i: number) => (
+        <FaqItem item={item} key={i} />
+      ))}
+    </div>
+  );
+};
+
+export default Faq;
