62b737d35d1558edb62158d80557be00b17f522f
musaakkayaa <musaakkayaa@gmail.com>
2023-06-12 22:58:45 +0300

    FRO-531, added animation

--- src/pages/Drop/components/Banner/Banner.tsx
+++ src/pages/Drop/components/Banner/Banner.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Img from "components/Img";
 
 import { useDropDetailContext } from "../../Detail/DetailContext";
@@ -7,8 +7,25 @@ import "./Banner.css";
 const Banner = () => {
   const { dropDetail } = useDropDetailContext();
 
+  const setScrollPosition = () => {
+    const offset = window.scrollY;
+    const offsetHeight = document.body.offsetHeight;
+    const innerHeight = window.innerHeight;
+    const scrollValue = String(offset / (offsetHeight - innerHeight));
+
+    document.body.style.setProperty("--scroll", scrollValue);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", setScrollPosition, false);
+
+    return () => {
+      window.removeEventListener("scroll", setScrollPosition, false);
+    };
+  });
+
   return (
-    <div className="banner-container">
+    <div className="banner-container animate">
       <Img className="banner-image" src={dropDetail.banner} />
     </div>
   );
