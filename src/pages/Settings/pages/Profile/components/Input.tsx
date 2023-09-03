0d433f9364058fd02c115bb8efa5b6fa1ceed9c3
musaakkayaa <musaakkayaa@gmail.com>
2023-09-03 18:07:51 +0300

    FRO-659 resolve feedback

--- src/pages/Settings/pages/Profile/components/Input.tsx
+++ src/pages/Settings/pages/Profile/components/Input.tsx
@@ -1,16 +1,8 @@
 import React from "react";
-import InputBase from "components/Input";
-import Label from "./Label";
-import HelperText from "./HelperText";
+import InputLabel from "components/InputLabel";
 
-const Input = ({ label, helperText, ...etc }: any) => {
-  return (
-    <div className="flex flex-col gap-2">
-      {label && <Label>{label}</Label>}
-      {helperText && <HelperText>{helperText}</HelperText>}
-      <InputBase {...etc} />
-    </div>
-  );
+const Input = (props: any, ref: any) => {
+  return <InputLabel {...props} ref={ref} />;
 };
 
-export default Input;
+export default React.forwardRef(Input);
