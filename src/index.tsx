eba95f4c97e16429d9cd4b517c42880396a4737f
okanakdemirr <okanakdemirr@gmail.com>
2023-09-10 23:18:13 +0300

    import hotfix

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import LOCALES from "./locales";
 import * as Sentry from "@sentry/react";
 import { BrowserTracing } from "@sentry/browser";
 
-import { configureChains, createConfig, mainnet, WagmiConfig } from "wagmi";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 // import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
