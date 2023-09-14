27b807b3d5f4c36f0f182aedfd6e221450df16f9
musaakkayaa <musaakkayaa@gmail.com>
2023-09-14 23:16:03 +0300

    FRO-687 rollback config

--- src/config/index.ts
+++ src/config/index.ts
@@ -14,7 +14,7 @@ class Config {
 
   constructor() {
     const hostname = window.location.hostname;
-    this.config = fuel;
+    this.config = base;
     if (hostname === WAGMI_URL) {
       this.config = base;
     } else if (hostname === FUEL_URL) {
