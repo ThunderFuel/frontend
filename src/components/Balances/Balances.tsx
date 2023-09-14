956b14271f653f869ea1da97d2718865031a71ab
okanakdemirr <okanakdemirr@gmail.com>
2023-09-14 23:08:11 +0300

    fetchbidbalance fix

--- src/components/Balances/Balances.tsx
+++ src/components/Balances/Balances.tsx
@@ -18,6 +18,7 @@ const Balances = (refresh: any) => {
   }
 
   function fetchBidBalance() {
+    if (user.walletAddress === undefined) return;
     getBidBalance({ contractAddress: user.walletAddress, user: user }).then((res) => {
       console.log({ res });
       setBidBalance(res);
