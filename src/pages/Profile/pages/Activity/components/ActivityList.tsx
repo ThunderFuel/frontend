9c006c379f6d18c8ff372bbc482481134d841667
musaakkayaa <musaakkayaa@gmail.com>
2023-09-26 23:51:37 +0300

    FRO-703, text-overflow

--- src/pages/Profile/pages/Activity/components/ActivityList.tsx
+++ src/pages/Profile/pages/Activity/components/ActivityList.tsx
@@ -6,7 +6,7 @@ import ActivityItems from "components/ActivityList/components/ActivityItems";
 const ActivityList = (props: any) => {
   return (
     <ActivityProvider value={props}>
-      <div className="px-5 flex h-full">
+      <div className="pl-5 flex h-full">
         <Sidebar className="w-60" hiddenTabOffset={true} />
         <ActivityItems />
       </div>
