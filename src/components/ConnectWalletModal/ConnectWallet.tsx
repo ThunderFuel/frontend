fe21dcd326002bc285628d4dcc47134c822955db
okanakdemirr <okanakdemirr@gmail.com>
2023-10-02 22:55:14 +0300

    FRO-708

--- src/components/ConnectWalletModal/ConnectWallet.tsx
+++ src/components/ConnectWalletModal/ConnectWallet.tsx
@@ -97,16 +97,20 @@ export const ConnectWallet = () => {
         })}
       </div>
 
-      <div className="flex p-[15px] gap-x-[15px] bg-gray border border-gray rounded-md mt-auto">
-        <IconLightning className="text-white w-5 h-5" />
-        <div className="flex flex-col gap-y-[5px]">
-          <h6 className="text-head6 font-spaceGrotesk text-white">New to Fuel?</h6>
-          <p className="text-bodyMd font-spaceGrotesk text-gray-light">Fuel is the fastest execution layer for the modular blockchain stack.</p>
-          <a className="text-bodyMd font-spaceGrotesk text-white underline" href="https://www.fuel.network/" target="_blank" rel="noreferrer">
-            Learn More
-          </a>
+      {config.getConfig("type") === "fuel" ? (
+        <div className="flex p-[15px] gap-x-[15px] bg-gray border border-gray rounded-md mt-auto">
+          <IconLightning className="text-white w-5 h-5" />
+          <div className="flex flex-col gap-y-[5px]">
+            <h6 className="text-head6 font-spaceGrotesk text-white">New to Fuel?</h6>
+            <p className="text-bodyMd font-spaceGrotesk text-gray-light">Fuel is the fastest execution layer for the modular blockchain stack.</p>
+            <a className="text-bodyMd font-spaceGrotesk text-white underline" href="https://www.fuel.network/" target="_blank" rel="noreferrer">
+              Learn More
+            </a>
+          </div>
         </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
