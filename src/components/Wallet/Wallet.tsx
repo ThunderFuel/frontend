ae2def9e0379c8da7ce50c69ab9e8cdc08e2e9b6
okanakdemirr <okanakdemirr@gmail.com>
2023-11-07 23:51:10 +0300

    console.log removed

--- src/components/Wallet/Wallet.tsx
+++ src/components/Wallet/Wallet.tsx
@@ -94,8 +94,6 @@ const Wallet = ({ show, onClose }: { show: boolean; onClose: any }) => {
     </div>
   );
 
-  console.log(`${config.getConfig("type") === "wagmi" ? lineaExplorerLink : fueldExplorerLink}${user?.walletAddress}`);
-
   return (
     <Modal className="cart" title="Wallet" onClose={onClose} show={show} footer={footer}>
       <div className="flex pl-5 pt-5 pb-[33px] gap-x-5 font-spaceGrotesk border-b border-gray">
