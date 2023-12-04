d66caa06c31562980e62f9ef67ce9ace5efa6d16
musaakkayaa <musaakkayaa@gmail.com>
2023-12-04 22:54:18 +0300

    FRO-1 fix tooltip

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import "./Tooltip.css";
 import clsx from "clsx";
 
@@ -12,22 +12,37 @@ interface ITooltip {
 
 const Tooltip = ({ children, position = "bottom", content, contentClass, hiddenArrow = false }: ITooltip) => {
   const ref = useRef<HTMLDivElement>(null);
-  const [elPosition, setElPosition] = useState<any>({});
-  const onMouseEnter = () => {
-    if (ref.current) {
+  const refContent = useRef<HTMLDivElement>(null);
+
+  const currentPosition = () => {
+    if (ref.current && refContent.current) {
       const x = ref.current?.getBoundingClientRect().x + ref.current.offsetWidth / 2;
       const y = ref.current?.getBoundingClientRect().y + ref.current?.getBoundingClientRect().height / 2 - 10;
-      setElPosition({
-        x,
-        y,
-      });
+      refContent.current.style.left = `${x}px`;
+      refContent.current.style.top = `${y}px`;
+    }
+  };
+  const onMouseEnter = () => {
+    if (ref.current && refContent.current) {
+      currentPosition();
+      refContent.current.style.visibility = "visible";
+      refContent.current.style.opacity = "1";
+    }
+  };
+  const onMouseLeave = () => {
+    if (refContent.current) {
+      refContent.current.style.opacity = "0";
     }
   };
 
+  useEffect(() => {
+    currentPosition();
+  }, []);
+
   return (
-    <div className={clsx("tooltip", position)} ref={ref} onMouseEnter={onMouseEnter}>
+    <div className={clsx("tooltip", position)} ref={ref} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {children}
-      <div className={clsx("content", contentClass)} style={{ left: elPosition.x, top: elPosition.y }}>
+      <div ref={refContent} className={clsx("content", contentClass)}>
         {content} {!hiddenArrow && <span className="arrow" />}
       </div>
     </div>
