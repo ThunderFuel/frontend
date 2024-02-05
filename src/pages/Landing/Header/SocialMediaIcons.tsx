17b8857e1d63fb3d5a163a59f4cdfd61a98a94e5
okanakdemirr <okanakdemirr@gmail.com>
2024-02-05 20:19:08 +0300

    discord link updated

--- src/pages/Landing/Header/SocialMediaIcons.tsx
+++ src/pages/Landing/Header/SocialMediaIcons.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { IconDiscord, IconSocial3Dots, IconTwitter } from "../../../icons";
+import { DISCORD_URL } from "global-constants";
 
 const SocialMediaIcons = () => {
   return (
     <ul className="flex gap-3 text-gray-light">
-      <a href="https://discord.gg/thundernftmarket" target="_blank" rel="noreferrer" className="hover:text-white">
+      <a href={DISCORD_URL} target="_blank" rel="noreferrer" className="hover:text-white">
         <IconDiscord />
       </a>
       <a href="https://twitter.com/ThunderbyFuel" target="_blank" rel="noreferrer" className="hover:text-white">
