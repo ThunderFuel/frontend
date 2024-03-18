b4620b78f1badcbbd2c685b7243beb2195b29765
okanakdemirr <okanakdemirr@gmail.com>
2024-03-18 23:56:32 +0300

    HttpClient api key removed

--- src/api/HttpClient.ts
+++ src/api/HttpClient.ts
@@ -31,7 +31,7 @@ export default class HttpClient {
 
   constructor(baseURL: string, options: HttpClientOptions = {}) {
     const { requestInterceptors = [], responseInterceptors = [], headers = {}, ...rest } = options;
-    headers.apikey = "0cf4e21b-7c80-45f3-bcb9-aefd48acfb14";
+    // headers.apikey = "0cf4e21b-7c80-45f3-bcb9-aefd48acfb14";
 
     this.CancelToken = axios.CancelToken;
 
