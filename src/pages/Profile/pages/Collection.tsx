8010ac4efa6bff55d678f33035965a645484ec79
musa.akkaya <musa.akkaya@trendyol.com>
2024-05-07 19:28:55 +0300

    FRO-1

--- src/pages/Profile/pages/Collection.tsx
+++ src/pages/Profile/pages/Collection.tsx
@@ -33,8 +33,9 @@ const Collection = () => {
   const [collectionItems, setCollectionItems] = useState<any>([]);
   const [pagination, setPagination] = useState<any>({});
   const initParams = getInitParams();
+  const isValidFilter = true;
 
-  const onChangeFilter = async (params: any) => {
+  const onChangeFilterFetchCollection = async (params: any) => {
     const { sortingType, search, ...etcParams } = params;
 
     try {
@@ -77,6 +78,63 @@ const Collection = () => {
       };
     } catch (e) {
       console.log(e);
+    }
+  };
+  const onChangeFilter = (params: any) => {
+    setIsLoading(true);
+    try {
+      setCollectionItems([]);
+
+      let tmpCollectionItems: any = userInfo.tokens;
+      Object.entries(params).forEach(([key, item]: any) => {
+        if (key === "search") {
+          tmpCollectionItems = tmpCollectionItems.filter((collectionItem: any) => String(collectionItem.name).toLowerCase().search(item) > -1);
+        }
+        if (key === "Status" && item?.type === 3) {
+          tmpCollectionItems = tmpCollectionItems.filter((collectionItem: any) => {
+            if (item.value === "1") {
+              return collectionItem.salable;
+            } else if (item.value === "2") {
+              return collectionItem.onAuction;
+            } else if (item.value === "3") {
+              return !collectionItem.salable && !collectionItem.onAuction;
+            } else if (item.value === "4") {
+              return collectionItem.hasOffer;
+            } else if (item.value === "6") {
+              return true;
+            }
+          });
+        }
+        if (key === "Collections" && item?.type === 6) {
+          tmpCollectionItems = tmpCollectionItems.filter((collectionItem: any) => item.value.includes(collectionItem.collectionId.toString()));
+        }
+      });
+      tmpCollectionItems = tmpCollectionItems.sort((a: any, b: any) => {
+        if (params.sortingType === 1) {
+          const aPrice = a.price ?? 99;
+          const bPrice = b.price ?? 99;
+
+          return aPrice - bPrice;
+        } else if (params.sortingType === 2) {
+          const aPrice = a.price ?? 0;
+          const bPrice = b.price ?? 0;
+
+          return bPrice - aPrice;
+        } else if (params.sortingType === 3) {
+          return a.listedTimeUnix - b.listedTimeUnix;
+        } else if (params.sortingType === 4) {
+          return a.higeshtSalePrice - b.higeshtSalePrice;
+        }
+
+        return a.lowestSalePrice - b.lowestSalePrice;
+      });
+      setCollectionItems(tmpCollectionItems);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 500);
     }
   };
 
@@ -160,7 +218,7 @@ const Collection = () => {
         initParams={initParams}
         filterItems={filter}
         options={{ ...options, ...profileOptions }}
-        onChangeFilter={onChangeFilter}
+        onChangeFilter={isValidFilter ? onChangeFilter : onChangeFilterFetchCollection}
       />
     </InfiniteScroll>
   );
