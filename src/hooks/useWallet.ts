789b0217eed2131343133bade6f3911da7e0ce74
okanakdemirr <okanakdemirr@gmail.com>
2024-08-19 19:48:36 +0300

    unnecessary api calls handled

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -29,7 +29,7 @@ export const useWallet = () => {
   }
 
   useEffect(() => {
-    if (isConnected && wallet && account) {
+    if (isConnected && wallet && account && user.walletAddress === undefined) {
       dispatch(setIsConnected(true));
       dispatch(setAddress(toB256(account as any) ?? ""));
 
@@ -42,7 +42,7 @@ export const useWallet = () => {
       dispatch(setIsConnected(false));
       dispatch(setUser({}));
     }
-  }, [isConnected, account, wallet]);
+  }, [isConnected, account, wallet, user]);
 
   const hasEnoughFunds = async (buyNowItemPrice?: any) => {
     return selectedGateway().hasEnoughFunds(buyNowItemPrice, getWalletAddress, user.walletAddress, totalAmount);
