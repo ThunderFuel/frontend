83885173c9db074079ea5f1522088b10a7b71c61
okanakdemirr <okanakdemirr@gmail.com>
2024-09-07 05:36:04 +0300

    Img deploy fix try

--- src/components/Img/Img.tsx
+++ src/components/Img/Img.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { AssetDefaultImageBg } from "assets";
 import clsx from "clsx";
 
@@ -6,11 +6,11 @@ interface IImage {
   src: string | undefined;
   defaultImage?: string;
   className?: string;
-
   [key: string]: any;
 }
 
-const Img = ({ src, defaultImage = AssetDefaultImageBg, className, ...etc }: IImage, ref: any) => {
+// Define the ForwardRefRenderFunction type
+const Img: React.ForwardRefRenderFunction<HTMLImageElement, IImage> = ({ src, defaultImage = AssetDefaultImageBg, className, ...etc }, ref) => {
   const onError = (e: any) => {
     e.target.src = defaultImage;
   };
@@ -18,4 +18,5 @@ const Img = ({ src, defaultImage = AssetDefaultImageBg, className, ...etc }: IIm
   return <img className={clsx("bg-gray", className)} ref={ref} src={src ?? defaultImage} loading="lazy" {...etc} onError={onError} />;
 };
 
-export default React.forwardRef(Img);
+// Use React.forwardRef with the Img component
+export default forwardRef(Img);
