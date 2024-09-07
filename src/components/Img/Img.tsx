4d8e0b6216c7944503a8a96569a6f644ebb3cbd4
okanakdemirr <okanakdemirr@gmail.com>
2024-09-07 14:01:45 +0300

    forward ref fix

--- src/components/Img/Img.tsx
+++ src/components/Img/Img.tsx
@@ -1,18 +1,18 @@
 import React, { forwardRef } from "react";
-import { AssetDefaultImageBg } from "assets";
+import { AssetDefaultImageBg } from "assets"; // Make sure this import is correct and points to the right file
 import clsx from "clsx";
 
 interface IImage {
-  src: string | undefined;
+  src?: string; // Make src optional since you handle undefined cases
   defaultImage?: string;
   className?: string;
-  [key: string]: any;
+  [key: string]: any; // Allow any other props
 }
 
 // Define the ForwardRefRenderFunction type
 const Img: React.ForwardRefRenderFunction<HTMLImageElement, IImage> = ({ src, defaultImage = AssetDefaultImageBg, className, ...etc }, ref) => {
-  const onError = (e: any) => {
-    e.target.src = defaultImage;
+  const onError = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    e.currentTarget.src = defaultImage;
   };
 
   return <img className={clsx("bg-gray", className)} ref={ref} src={src ?? defaultImage} loading="lazy" {...etc} onError={onError} />;
