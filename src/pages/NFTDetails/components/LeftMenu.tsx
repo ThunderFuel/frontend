925c3edde93f2e30e007c15c0b712e428dae70f1
okanakdemirr <okanakdemirr@gmail.com>
2024-10-08 22:56:45 +0300

    issue #13

--- src/pages/NFTDetails/components/LeftMenu.tsx
+++ src/pages/NFTDetails/components/LeftMenu.tsx
@@ -484,7 +484,7 @@ const LeftMenu = (props: any) => {
                 </div>
                 <div className="flex flex-col gap-y-[5px]">
                   <div className="text-headline-01 text-gray-light">CREATOR FEE</div>
-                  {nft?.royalty}%
+                  {nft?.royalty ?? 0}%
                 </div>
               </Box>
             </div>
